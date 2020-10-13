a3128dd8ac529934cdedb421353e480330f40902
Justin Kennedy <Justin.L.Kennedy@outlook.com>
2020-10-12 21:44:08 -0500

    Revert "Contact Form complete"
    This reverts commit e1f6ab33f279c37a035df889721b3bb6e3d6e7e0.

--- StoreFront.UI.MVC/Controllers/HomeController.cs
+++ StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Web.Mvc;
-using StoreFront.UI.MVC.Models;
-using System.Net.Mail;
-using System.Net;
+﻿using System.Web.Mvc;
 
 namespace StoreFront.UI.MVC.Controllers
 {
@@ -26,37 +22,9 @@ namespace StoreFront.UI.MVC.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Contact(ContactViewModel cvm)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(cvm);
-            }
+            ViewBag.Message = "Your contact page.";
 
-            string emailBody = $"You have received an email from {cvm.Name} with a subject {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br /> <br />{cvm.Message}";
-            MailMessage msg = new MailMessage(
-                "no-reply@justinlkennedy.com",
-                "justin.l.kennedy@outlook.com",
-                "Email from StoreFront.JustinLKennedy.com",
-                emailBody);
-            msg.IsBodyHtml = true;
-            msg.Priority = MailPriority.High;
-            SmtpClient client = new SmtpClient("mail.justinlkennedy.com");
-            client.Credentials = new NetworkCredential("no-reply@justinlkennedy.com", "C3ntr!q");
-            try
-            {
-                client.Send(msg);
-            }
-            catch (Exception ex)
-            {
-                ViewBag.ErrorMessage = $"Sorry, something went wrong. Error message: {ex.Message}<br />{ex.StackTrace}";
-                return View(cvm);
-            }
-            return View("EmailConfirmation", cvm);
+            return View();
         }
     }
 }
